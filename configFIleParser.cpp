b0ef379e19e02cfc4adc8b239318e2014968df2e
Mohamed ELazhari <mazhari@student.1337.ma>
2023-02-25 19:16:08 +0100

    checking is there a  ServerBlock and starting  to pars it

--- configFIleParser.cpp
+++ configFIleParser.cpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:01:45 by mazhari           #+#    #+#             */
-/*   Updated: 2023/02/25 17:28:42 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/02/25 19:12:54 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,27 @@ void removeWhiteSpace(std::string &str){
 void PrintExit(std::string str){
     std::cerr << str << std::endl;
     exit(1);
+}
+
+// void parsLine(std::string str){
+//     size_t      pos;
+//     std::string key;
+//     std::string value;
+    
+//     for (pos = 0; isspace(str[pos]); pos++);
+//     key = str.substr(pos, str.find_first_of(" \t", pos));
+//     pos = str.find_first_of(" \t", pos);
+//     value = str.substr(str.find_first_not_of(" \t", pos), str.length() - 1);
+//     std::cout << key << std::endl;
+//     std::cout << value << std::endl;
+// }
+
+void parsServerBlock(std::string content){
+    std::string     tmp;
+    if (content.find(';') == std::string::npos)
+        PrintExit("Error: No semicolon");
+    tmp = content.substr(0, content.find(';'));
+    // parsLine(tmp);
 }
 
 void    parsConfigFile(std::string fileName)
@@ -53,18 +74,14 @@ void    parsConfigFile(std::string fileName)
         }
         content.append(line);
     }
-    // // check if file is empty
-    // if (content.empty())
-    //     PrintExit("Error: File is empty");
-    // // cheking if there is a server block
-    // if (content.find("{") != std::string::npos)
-    //     tmp = content.substr(0, content.find("{") + 1);
-    // removeWhiteSpace(tmp);
-    // if (tmp != "server{")
-    //     PrintExit("Error: No server block");
-    // // check syntax error of {}
-    // if (std::count(content.begin(), content.end(), '{') != std::count(content.begin(), content.end(), '}')
-    //     && content[content.length() - 1] != '}')
-    //     PrintExit("Error: Syntax error in {}");
-    // // parsServerBlock(content);
+    // check if file is empty
+    if (content.empty())
+        PrintExit("Error: File is empty");
+    // cheking if there is a server block
+    if (content.find("{") != std::string::npos)
+        tmp = content.substr(0, content.find("{") + 1);
+    removeWhiteSpace(tmp);
+    if (tmp != "server{")
+        PrintExit("Error: No server block");
+    parsServerBlock(content.substr(content.find("{") + 1, content.length() - 1));
 }
