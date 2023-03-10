c1eaaa3127e317e2d144569bbe87bd1b98cd5333
Mohamed ELazhari <mazhari@student.1337.ma>
2023-03-10 16:12:00 +0100

    ready to merge

--- includes/server.hpp
+++ includes/server.hpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 19:15:21 by mazhari           #+#    #+#             */
-/*   Updated: 2023/03/09 17:43:14 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/03/10 16:07:49 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,19 @@ class server: public Values
 {
 	private:
 		std::map<std::string, location>		_locations;
+		// request								_request;
+		// response							_response;
+		// client
 	public:
 		server(parsConfig &config);
 		~server();
 
 		void setValues(std::string &key, std::string &value);
 		void setDefaultValues();
+		// execPost();
+		// execGET();
+		// execDELEt();
+		// startserver();
 
 		void printValues();
 };
