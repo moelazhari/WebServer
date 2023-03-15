cb09517d45c219d38aa315eb4d6e10437e9be524
Mohamed ELazhari <mazhari@student.1337.ma>
2023-03-15 17:10:50 +0100

    adding getters for class location

--- includes/location.hpp
+++ includes/location.hpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 17:01:39 by mazhari           #+#    #+#             */
-/*   Updated: 2023/03/14 19:26:00 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/03/15 17:08:29 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 class location : public Values
 {
 	private:
-		std::map<std::string, std::string>	_cgiPath;
+		std::map<std::string, std::string>	_cgiPaths;
 		std::pair<int, std::string>			_return;
 	public:
 		location();
@@ -27,10 +27,12 @@ class location : public Values
 		~location();
 
 		void    setValues(std::string &key, std::string &value);
-		void    checkValues();
 		
-		void setCgiPath(std::string key, std::string &value);
+		void setCgiPaths(std::string key, std::string &value);
 		void setReturn(std::string key, std::string &value);
+
+		std::map<std::string, std::string>	getCgiPaths();
+		std::pair<int, std::string>			getReturn();
 
 		void printValues();
 };
--- src/server/location.cpp
+++ src/server/location.cpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 19:05:47 by mazhari           #+#    #+#             */
-/*   Updated: 2023/03/14 19:37:52 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/03/15 17:09:50 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ location::location(){
     return ;
 }
 
-location::location (std::string &content): _cgiPath(std::map<std::string, std::string>()), _return(std::pair<int, std::string>(0, "")){
+location::location (std::string &content): _cgiPaths(std::map<std::string, std::string>()), _return(std::pair<int, std::string>(0, "")){
     size_t      			pos = 0;
     std::string             tmp;
     std::string             key;
@@ -41,7 +41,7 @@ void    location::setValues(std::string &key, std::string &value){
     std::string values[11] = {"client_max_body_size",  "autoindex", "host", "root", "error_page", "allow_methods", "index", "path_info", "return"};
 
     void (location::*f[11])(std::string key, std::string &value) = {&location::setClientMaxBodySize, &location::setAutoIndex, &location::setHost,\
-    &location::setRoot, &location::setErrorPages, &location::setAllowMethods, &location::setIndexs, &location::setCgiPath, &location::setReturn};
+    &location::setRoot, &location::setErrorPages, &location::setAllowMethods, &location::setIndexs, &location::setCgiPaths, &location::setReturn};
     
     for (int i = 0; i < 11; i++){
         if (key == values[i]){
@@ -52,7 +52,7 @@ void    location::setValues(std::string &key, std::string &value){
     PrintExit("Error config file in key " + key + ": invalid key");
 }
 
-void  location::setCgiPath(std::string key, std::string &value){
+void  location::setCgiPaths(std::string key, std::string &value){
     std::vector<std::string>    tmp = split(value, " ");
 
     if (tmp.size() != 2)
@@ -61,9 +61,9 @@ void  location::setCgiPath(std::string key, std::string &value){
         PrintExit("Error config file in key " + key + ": " + tmp[0] + " is not valid extension");
     if (tmp[0] != ".py" && tmp[0] != ".php")
         PrintExit("Error config file in key " + key + ": " + tmp[0] + " extension is not supported yet");
-    if (_cgiPath.find(tmp[0]) != _cgiPath.end())
+    if (_cgiPaths.find(tmp[0]) != _cgiPaths.end())
         PrintExit("Error config file in key " + key + ": " + tmp[0] + " extension is already set");
-    this->_cgiPath[tmp[0]] = tmp[1];
+    this->_cgiPaths[tmp[0]] = tmp[1];
 }
 
 void location::setReturn(std::string key, std::string &value){
@@ -75,6 +75,15 @@ void location::setReturn(std::string key, std::string &value){
         PrintExit("Error config file in key " + key + ": " + tmp[0] + " is not valid error code");
     this->_return = std::pair<int, std::string>(toInt(tmp[0]), tmp[1]);
 }
+
+std::map<std::string, std::string> location::getCgiPaths(){
+    return this->_cgiPaths;
+}
+
+std::pair<int, std::string> location::getReturn(){
+    return this->_return;
+}
+
 void location::printValues(){
     std::cout << "client_max_body_size: " << this->_clientMaxBodySize << std::endl;
     std::cout << "autoindex: " << this->_autoIndex << std::endl;
@@ -92,7 +101,7 @@ void location::printValues(){
     std::cout << std::endl;
     //print cgi path
     std::cout << "cgi path: ";
-    for (std::map<std::string, std::string>::iterator it = this->_cgiPath.begin(); it != this->_cgiPath.end(); it++)
+    for (std::map<std::string, std::string>::iterator it = this->_cgiPaths.begin(); it != this->_cgiPaths.end(); it++)
         std::cout << it->first << " " << it->second << " ";
 }
 
