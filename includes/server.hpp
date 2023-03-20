e540c84e41152e2fd3ea6b0d1db9a403427b6ff5
Mohamed ELazhari <mazhari@student.1337.ma>
2023-03-20 19:52:20 +0000

    add meme_types to server class

--- includes/server.hpp
+++ includes/server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 19:15:21 by mazhari           #+#    #+#             */
-/*   Updated: 2023/03/19 18:59:35 by aboudoun         ###   ########.fr       */
+/*   Updated: 2023/03/20 19:25:01 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,21 @@ class server: public Values
 {
 	private:
 		std::map<std::string, location>		_locations;
+		std::map<std::string, std::string>	_memeTypes;
 	public:
 		server(parsConfig &config);
 		~server();
-
+		
 		void setValues(std::string &key, std::string &value);
 		void setDefaultValues();
+		//setters
+		void	setMemeTypes(std::string path);
+		
+		// getters
 		std::string							getHost();
 		std::vector<int>					getPorts();
 		std::map<std::string, location>		&getLocations();
+		
 		void printValues();
 };
 
