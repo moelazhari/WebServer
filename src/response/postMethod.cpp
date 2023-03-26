de3567432c81aacadc61c650d904fb41f0dbfec0
abderrahim <abderrahimboudounit@gmail.com>
2023-03-26 03:24:47 +0000

    adding post and get method

--- src/response/postMethod.cpp
+++ src/response/postMethod.cpp
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 15:38:20 by aboudoun          #+#    #+#             */
-/*   Updated: 2023/03/26 01:20:07 by aboudoun         ###   ########.fr       */
+/*   Updated: 2023/03/26 03:23:33 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,57 @@
 
 void response::Post(server& serv, ParseRequest& request)
 {
+
 	std::string path;
 
 	path = this->getLocation().getRoot();
 	path = joinPaths(path, request.getLink().substr(this->getLocationPath().size()));
-	return;
 	// if (/*location supports upload*/)
 	// {
-	// 	// upload file
+	// 	std::ifstream file(path);
+
+	// 	file << request.getBody();
+		
 	// }
-	// else if (!is_file(path) && !is_dir(path))
-	// {
-	// 	this->setStatus("Not Found", 404);
-	// 	this->setFilePath("./error_pages/404.html");
-	// 	this->fillResponse(serv);
-	// }
-	// else if (is_dir(path))
-	// {
-	// 	if (this->getLocation().getIndexs().empty())
-	// 	{
-	// 		// TODO do i need to check if the index is present in the directory? or just pss it to the cgi?
-	// 		this->setStatus("Forbidden", 403);
-	// 		this->setFilePath("./error_pages/403.html");
-	// 		this->fillResponse(serv);
-	// 	}
-	// 	else if (this->getLocation().getCgiPaths().empty())
-	// 	{
-	// 		this->setStatus("Forbidden", 403);
-	// 		this->setFilePath("./error_pages/403.html");
-	// 		this->fillResponse(serv);
-	// 	}
-	// 	else
-	// 	{
-	// 		//run cgi on requested file with Post method
-	// 	}
-	// }
-	// else if (is_file(path))
-	// {
-	// 	if (this->getLocation().getCgiPaths().empty())
-	// 	{
-	// 		this->setStatus("Forbidden", 403);
-	// 		this->setFilePath("./error_pages/403.html");
-	// 		this->fillResponse(serv);
-	// 	}
-	// 	else
-	// 	{
-	// 		//run cgi on requested file with Post method
-	// 	}
-	// }
+	if (!is_file(path) && !is_dir(path))
+	{
+		this->setStatus("Not Found", 404);
+		this->setFilePath("./error_pages/404.html");
+		this->fillResponse(serv);
+	}
+	else if (is_dir(path))
+	{
+		if (this->getLocation().getIndexs().empty())
+		{
+			// TODO do i need to check if the index is present in the directory? or just pss it to the cgi?
+			this->setStatus("Forbidden", 403);
+			this->setFilePath("./error_pages/403.html");
+			this->fillResponse(serv);
+		}
+		else if (this->getLocation().getCgiPaths().empty())
+		{
+			std::cout << "run cgi" << std::endl;
+			this->setStatus("Forbidden", 403);
+			this->setFilePath("./error_pages/403.html");
+			this->fillResponse(serv);
+		}
+		else
+		{
+			//run cgi on requested file with Post method
+		}
+	}
+	else if (is_file(path))
+	{
+		if (this->getLocation().getCgiPaths().empty())
+		{
+			std::cout << "run cgi" << std::endl;
+			this->setStatus("Forbidden", 403);
+			this->setFilePath("./error_pages/403.html");
+			this->fillResponse(serv);
+		}
+		else
+		{
+			//run cgi on requested file with Post method
+		}
+	}
 }
